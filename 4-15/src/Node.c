70830e9ff35a2b3acbc3166eb4a22936623c117e
Connor McMahon <mcmahon@cs.unc.edu>
2026-04-15 12:28:48 +0000

    add starter code for 4-15

---
+++ 4-15/src/Node.c
@@ -0,0 +1,42 @@
+#include "Node.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Guards.h"
+
+Node* CharNode_new(char c) {
+    Node* node = OOM_GUARD(malloc(sizeof(Node)), __FILE__, __LINE__);
+    node->type = CHAR_NODE;
+    node->data.value = c;
+    return node;
+};
+
+Node* PairNode_new(Node* left, Node* right) {
+    Node* node = OOM_GUARD(malloc(sizeof(Node)), __FILE__, __LINE__);
+    node->type = PAIR_NODE;
+    node->data.pair.left = left;
+    node->data.pair.right = right;
+    return node;
+}
+
+Node* ErrorNode_new(const char* msg) {
+    Node* node = OOM_GUARD(malloc(sizeof(Node)), __FILE__, __LINE__);
+    node->type = ERROR_NODE;
+    node->data.error = msg;
+    return node;
+}
+
+void* Node_drop(Node* self) {
+    switch (self->type) {
+        case ERROR_NODE:
+        case CHAR_NODE:
+            break;
+        case PAIR_NODE:
+            Node_drop(self->data.pair.left);
+            Node_drop(self->data.pair.right);
+            break;
+    }
+    free(self);
+    return NULL;
+};
